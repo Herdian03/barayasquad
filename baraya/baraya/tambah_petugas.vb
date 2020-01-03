a113c3829149fc3993083c5eb55d8dbd6c96ff6f
herdian <herdiansyahfirdaus9@gmail.com>
2020-01-03 09:30:05 +0700

    hanya test hapus database kendaraan

---
+++ baraya/baraya/tambah_petugas.vb
@@ -0,0 +1,9 @@
+﻿Imports MySql.Data.MySqlClient
+Imports baraya.koneksi
+
+Public Class tambah_petugas
+
+    Private Sub tambah_petugas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
